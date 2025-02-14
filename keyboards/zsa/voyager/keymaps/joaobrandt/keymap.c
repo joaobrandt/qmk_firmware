a7983d44772be748da20b6cb5824bba1a9607fcd
João Paulo G. Brandt <joao@brandt.ws>
2025-02-14 09:46:25 -0300

    Format the file

--- keyboards/zsa/voyager/keymaps/joaobrandt/keymap.c
+++ keyboards/zsa/voyager/keymaps/joaobrandt/keymap.c
@@ -6,21 +6,21 @@
 #define ML_SAFE_RANGE SAFE_RANGE
 
 enum custom_keycodes {
-  RGB_SLD = ML_SAFE_RANGE,
-  ST_MACRO_0,
-  ST_MACRO_1,
-  ST_MACRO_2,
-  ST_MACRO_3,
-  ST_MACRO_4,
-  ST_MACRO_5,
-  ST_MACRO_6,
-  ST_MACRO_7,
-  ST_MACRO_8,
-  ST_MACRO_9,
-  ST_MACRO_10,
-  ST_MACRO_11,
-  ST_MACRO_12,
-  ST_MACRO_13,
+    RGB_SLD = ML_SAFE_RANGE,
+    ST_MACRO_0,
+    ST_MACRO_1,
+    ST_MACRO_2,
+    ST_MACRO_3,
+    ST_MACRO_4,
+    ST_MACRO_5,
+    ST_MACRO_6,
+    ST_MACRO_7,
+    ST_MACRO_8,
+    ST_MACRO_9,
+    ST_MACRO_10,
+    ST_MACRO_11,
+    ST_MACRO_12,
+    ST_MACRO_13,
 };
 
 /* ------------------------------------- */
@@ -36,6 +36,7 @@ enum custom_keycodes {
 #define HR_SFT_SCLN MT(MOD_LSFT, KC_SCLN)
 #define SUPER_M LALT(LGUI(LCTL(LSFT(KC_M))))
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
 //╭─────────────────┬─────────────────┬─────────────────┬─────────────────┬─────────────────┬─────────────────╮ ╭─────────────────┬─────────────────┬─────────────────┬─────────────────┬─────────────────┬─────────────────╮
@@ -103,6 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*                                                                            _______,          _______,            _______,          ___*/
 /*//                                                                        ╰─────────────────┴─────────────────╯ ╰─────────────────┴─────────────────╯*/
 
+// clang-format off
+
 /* ------------------------------------- */
 /*          Key overrides                */
 /* ------------------------------------- */
@@ -125,6 +128,8 @@ extern rgb_config_t rgb_matrix_config;
 #define RED   {0,255,255}
 #define YELLW {41,255,255}
 
+
+// clang-format off
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [0] = {
         _____, _____, _____, _____, _____, _____,
@@ -175,134 +180,138 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         _____, _____,
     },
 };
+// clang-format on
 
 void set_layer_color(int layer) {
-  for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
-    HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
-    };
-    if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color(i, 0, 0, 0);
-    } else {
-        RGB rgb = hsv_to_rgb(hsv);
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
+    for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+        HSV hsv = {
+            .h = pgm_read_byte(&ledmap[layer][i][0]),
+            .s = pgm_read_byte(&ledmap[layer][i][1]),
+            .v = pgm_read_byte(&ledmap[layer][i][2]),
+        };
+        if (!hsv.h && !hsv.s && !hsv.v) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        } else {
+            RGB   rgb = hsv_to_rgb(hsv);
+            float f   = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+            rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
+        }
     }
-  }
 }
 
 bool rgb_matrix_indicators_user(void) {
-  if (rawhid_state.rgb_control) { return false; }
-  if (keyboard_config.disable_layer_led) { return false; }
+    if (rawhid_state.rgb_control) {
+        return false;
+    }
+    if (keyboard_config.disable_layer_led) {
+        return false;
+    }
 
-  switch (biton32(layer_state)) {
-    case 0:
-      set_layer_color(0);
-      break;
-    case 1:
-      set_layer_color(1);
-      break;
-    case 2:
-      set_layer_color(2);
-      break;
-    case 3:
-      set_layer_color(3);
-      break;
-   default:
-    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-      rgb_matrix_set_color_all(0, 0, 0);
-    break;
-  }
-  return true;
+    switch (biton32(layer_state)) {
+        case 0:
+            set_layer_color(0);
+            break;
+        case 1:
+            set_layer_color(1);
+            break;
+        case 2:
+            set_layer_color(2);
+            break;
+        case 3:
+            set_layer_color(3);
+            break;
+        default:
+            if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(0, 0, 0);
+            break;
+    }
+    return true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case ST_MACRO_0:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_A)));
-    }
-    break;
-    case ST_MACRO_1:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_6)) SS_DELAY(100) SS_TAP(X_O));
-    }
-    break;
-    case ST_MACRO_2:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_O));
-    }
-    break;
-    case ST_MACRO_3:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(100) SS_TAP(X_O));
-    }
-    break;
-    case ST_MACRO_4:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_U));
-    }
-    break;
-    case ST_MACRO_5:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_GRAVE) SS_DELAY(100) SS_TAP(X_A));
-    }
-    break;
-    case ST_MACRO_6:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_6)) SS_DELAY(100) SS_TAP(X_A));
-    }
-    break;
-    case ST_MACRO_7:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_A));
-    }
-    break;
-    case ST_MACRO_8:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(100) SS_TAP(X_A));
-    }
-    break;
-    case ST_MACRO_9:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_C));
-    }
-    break;
-    case ST_MACRO_10:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_6)) SS_DELAY(100) SS_TAP(X_E));
-    }
-    break;
-    case ST_MACRO_11:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_E));
-    }
-    break;
-    case ST_MACRO_12:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_I));
-    }
-    break;
-    case ST_MACRO_13:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_4))));
-    }
-    break;
+    switch (keycode) {
+        case ST_MACRO_0:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_TAP(X_A)));
+            }
+            break;
+        case ST_MACRO_1:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_TAP(X_6)) SS_DELAY(100) SS_TAP(X_O));
+            }
+            break;
+        case ST_MACRO_2:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_O));
+            }
+            break;
+        case ST_MACRO_3:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(100) SS_TAP(X_O));
+            }
+            break;
+        case ST_MACRO_4:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_U));
+            }
+            break;
+        case ST_MACRO_5:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_GRAVE) SS_DELAY(100) SS_TAP(X_A));
+            }
+            break;
+        case ST_MACRO_6:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_TAP(X_6)) SS_DELAY(100) SS_TAP(X_A));
+            }
+            break;
+        case ST_MACRO_7:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_A));
+            }
+            break;
+        case ST_MACRO_8:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(100) SS_TAP(X_A));
+            }
+            break;
+        case ST_MACRO_9:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_C));
+            }
+            break;
+        case ST_MACRO_10:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_TAP(X_6)) SS_DELAY(100) SS_TAP(X_E));
+            }
+            break;
+        case ST_MACRO_11:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_E));
+            }
+            break;
+        case ST_MACRO_12:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_I));
+            }
+            break;
+        case ST_MACRO_13:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_4))));
+            }
+            break;
 
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-  }
-  return true;
+        case RGB_SLD:
+            if (record->event.pressed) {
+                rgblight_mode(1);
+            }
+            return false;
+    }
+    return true;
 }
 
 /* ------------------------------------- */
 /* Keyboard initialization code          */
 /* ------------------------------------- */
 void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
+    rgb_matrix_enable();
 }
